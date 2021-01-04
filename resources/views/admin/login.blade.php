68be80fbfbe4c5d6d7e2eac02064f146bf8c732b
ngovantu96 <ngutrungwetoi@gmail.com>
2021-01-04 14:50:02 +0700

    dpadate

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -7,7 +7,7 @@
 
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="{{ asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback') }}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- icheck bootstrap -->
