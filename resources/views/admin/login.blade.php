d1d8aafdd4e9e663bfda245498cb345fe86fd7be
ngovantu96 <ngutrungwetoi@gmail.com>
2021-01-04 14:20:38 +0700

    dpadate

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin | Log in</title>
 
+
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="{{ asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback') }}">
     <!-- Font Awesome -->
