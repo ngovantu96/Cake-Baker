0c94cece4fdc594c6fa8e965df56f69d7a46695a
ngovantu96 <ngutrungwetoi@gmail.com>
2022-05-20 15:41:33 +0700

    change file index

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -6,10 +6,11 @@ namespace App\Http\Controllers;
 class DemoController extends Controller
 {
     public function index(){
-
+        $i = 0;
+        $number = $i + 1;
     }
     public function destroy()
     {
-        
+
     }
 }
