855ab5c2bb65bd3fb7cc78841456cd8762f0b396
ngovantu96 <ngutrungwetoi@gmail.com>
2022-05-20 15:33:03 +0700

    coomit function create and update

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -6,6 +6,15 @@ namespace App\Http\Controllers;
 class DemoController extends Controller
 {
     public function index(){
+
+    }
+
+    public function create()
+    {
+
+    }
+    public function update()
+    {
         
     }
 }
