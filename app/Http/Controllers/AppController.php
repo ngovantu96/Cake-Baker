cb36edd1dfbf7e126638eeaeffc0939dfa61c8a8
tungo96 <tu.ngovan@hdwebsoft.dev>
2023-06-17 08:55:01 +0700

    fix

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -7,6 +7,8 @@ class AppController extends Controller {
 
     public function index(){
         $a = 10;
+        $c = 50;
+        $f =30;
         return $a;
     }
     public function create(){
